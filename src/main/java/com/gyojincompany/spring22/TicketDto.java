e388d790a54b42d675147fe9c2cab6b0157858a8
ici <ici@DESKTOP-8CT0OFV>
2022-11-03 12:13:21 +0900

    트렌젝션 연습 적용전

---
+++ src/main/java/com/gyojincompany/spring22/TicketDto.java
@@ -0,0 +1,26 @@
+package com.gyojincompany.spring22;
+
+public class TicketDto {
+
+	private String consumerid;
+	private String amount;
+	
+	
+	
+	
+	
+	public String getConsumerid() {
+		return consumerid;
+	}
+	public void setConsumerid(String consumerid) {
+		this.consumerid = consumerid;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	
+}
